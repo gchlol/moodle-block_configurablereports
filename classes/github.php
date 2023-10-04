caca3cb82336b83222dc9722e98e4cdfa44cfd31
Nicholas Lambell <nicholas.lambell@health.qld.gov.au>
2023-10-04 15:15:38 +1000

    Add setting to provide a GitHub token which is used with requests

--- classes/github.php
+++ classes/github.php
@@ -4,6 +4,18 @@ namespace block_configurable_reports;
 class github extends \curl {
 	protected $repo = '';
 
+    /**
+     * @inheritDoc
+     */
+    public function __construct($settings = []) {
+        parent::__construct($settings);
+
+        $token = get_config('block_configurable_reports', 'repositorytoken');
+        if (!empty($token)) {
+            $this->set_token($token);
+        }
+    }
+
 	public function set_repo($repo) {
 		$this->repo = $repo;
 	}
@@ -19,6 +31,18 @@ class github extends \curl {
 		$this->setHeader('Authorization:'. $value);
 		return true;
 	}
+
+    /**
+     * Set an authorisation token to use with requests.
+     * This should be a fine-grained or classic personal access token.
+     *
+     * @param string $token Access token.
+     * @return void
+     */
+    public function set_token(string $token): void {
+        /** @noinspection PhpParamsInspection */
+        $this->setHeader("Authorization: Bearer $token");
+    }
 
 	public function get($endpoint, $params = array(), $options = array()) {
 		$url = 'https://api.github.com/repos/';
