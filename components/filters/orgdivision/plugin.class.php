275b53bfee58e58d86973f9a3a6a1ecb3c64ee48
Nicholas Lambell <nicholas.lambell@health.qld.gov.au>
2024-07-02 14:13:38 +1000

    GS-100: Update org division filter to use specific level fetching logic

--- components/filters/orgdivision/plugin.class.php
+++ components/filters/orgdivision/plugin.class.php
@@ -1,6 +1,7 @@
 <?php
 
 use block_configurable_reports\local\plugin\org_filter_base;
+use tool_organisation\persistent\level;
 
 /** @noinspection PhpUnused */
 class plugin_orgdivision extends org_filter_base {
@@ -25,6 +26,34 @@ class plugin_orgdivision extends org_filter_base {
      * @inheritDoc
      */
     public function print_filter(MoodleQuickForm $mform, stdClass $data): void {
-        parent::add_level_selector($mform, 'paypoint', 1, true);
+        self::add_level_selector($mform, 'unit', null, true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected static function get_levels_for_selector(int $hierarchy_id, ?int $depth): array {
+        global $DB;
+
+        $qh_unit = level::get_record([ 'idnumber' => '70071658' ]);
+        $reporting_parents = explode(",", get_config('local_lol', 'repunitparents'));
+
+        $parent_ids = $reporting_parents;
+        $parent_ids[] = $qh_unit->get('id');
+        if (empty($parent_ids)) {
+            return [];
+        }
+
+        [ $parent_sql, $params ] = $DB->get_in_or_equal($parent_ids, SQL_PARAMS_NAMED);
+        $params['hierarchy'] = $hierarchy_id;
+
+        return level::get_records_select(
+            "
+                hierarchyid = :hierarchy and
+                parent $parent_sql
+            ",
+            $params,
+            'name'
+        );
     }
 }
