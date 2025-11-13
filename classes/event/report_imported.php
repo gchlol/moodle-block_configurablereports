907cab5cb9391e7abadcef815041f9204a0f1aa4
Jonas Sajonas <Jonas.Sajonas@health.qld.gov.au>
2025-11-13 10:03:53 +1000

    GS-946: added new classes for events, added new local helper file

---
+++ classes/event/report_imported.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace block_configurable_reports\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+use core\event\base;
+use moodle_url;
+
+/**
+ * Event triggered when a configurable report is imported.
+ *
+ * @package     block_configurable_reports
+ * @copyright   2025 Gold Coast Health
+ * @author      Jonas Sajonas
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class report_imported extends base {
+    protected function init(): void {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_TEACHING;
+        $this->data['objecttable'] = 'block_configurable_reports';
+    }
+
+    public static function get_name(): string {
+        return get_string('event:reportimported', 'block_configurable_reports');
+    }
+
+    public function get_description(): string {
+        $data = (object) [
+            'userid' => $this->userid,
+            'reportname' => $this->other['reportname'] ?? '',
+            'objectid' => $this->objectid,
+            'source' => $this->other['source'] ?? 'unknown',
+        ];
+        return get_string('event:reportimported:desc', 'block_configurable_reports', $data);
+    }
+
+    public function get_url(): moodle_url {
+        return new moodle_url('/blocks/configurable_reports/editreport.php', ['id' => $this->objectid]);
+    }
+}
